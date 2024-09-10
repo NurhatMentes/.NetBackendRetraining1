3e0b4180f2b288fd1578741d11f5edf5380232b0
Nurhat Menteş <nurhatmentes@gmail.com>
2024-09-10 12:06:55 +0300

    Add project files.

---
+++ ConsoleUA/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+
+ProductManager productManager = new ProductManager(new InMemoryProductDal());
+
+foreach (var item in productManager.GetAll())
+{
+    Console.WriteLine(item.ProductName);
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,57 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.InMemory
+{
+    //InMemory bellekte yazılacak veri erişim kodları demek
+    public class InMemoryProductDal : IProductDal
+    {
+        List<Product> _products;
+
+        //ctor, bellekte referans aldığı zaman çalışacak kod bloğudur
+        public InMemoryProductDal()
+        {
+            _products = new List<Product>
+            {
+                new Product { ProductId = 1, CategoryId = 1, ProductName = "Bardak", UnitPrice = 10, UnitsInStock = 15 },
+                new Product { ProductId = 2, CategoryId = 1, ProductName = "Kamera", UnitPrice = 500, UnitsInStock = 3 },
+                new Product { ProductId = 3, CategoryId = 2, ProductName = "Telefon", UnitPrice = 1500, UnitsInStock = 2 },
+                new Product { ProductId = 4, CategoryId = 2, ProductName = "Klavye", UnitPrice = 150, UnitsInStock = 65 }
+            };
+        }
+
+        public void Add(Product product)
+        {
+           _products.Add(product);
+        }
+
+        public void Delete(Product product)
+        {
+             _products.Remove(_products.SingleOrDefault(p=>p.ProductId==product.ProductId));
+        }
+
+        public List<Product> GetAll()
+        {
+            return _products;
+        }
+
+        public List<Product> GetAllByCategory(int categoryId)
+        {
+            return _products.Where(p=>p.CategoryId==categoryId).ToList();
+        }
+
+        public void Update(Product product)
+        {
+           Product productToUpdate = _products.SingleOrDefault(_ => _.ProductId==product.ProductId);
+            productToUpdate.ProductName = product.ProductName;
+            productToUpdate.UnitPrice = product.UnitPrice; 
+            productToUpdate.UnitsInStock = product.UnitsInStock;
+            productToUpdate.CategoryId = product.CategoryId;
+        }
+    }
+}

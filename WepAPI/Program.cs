4baf068f3fe3fbd7a296364ea78440bcc91280bb
Nurhat Menteş <nurhatmentes@gmail.com>
2024-09-11 16:56:36 +0300

    Retraining commit 4 -Autofac -FluentValidation -Interceptors

--- WepAPI/Program.cs
+++ WepAPI/Program.cs
@@ -2,19 +2,35 @@ using Business.Abstract;
 using Business.Concrete;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Autofac.Extras.DynamicProxy;
+using Business.DependencyResolves.Autofac;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// ASP.NET Core default DI container'ýný Autofac ile deðiþtirdik
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+
+builder.Host.ConfigureContainer<ContainerBuilder>(containerBuilder =>
+{
+    // AutofacBusinessModule'i burada kaydetdik
+    containerBuilder.RegisterModule(new AutofacBusinessModule());
+});
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-//iac
-builder.Services.AddScoped<IProductService, ProductManager>();
-builder.Services.AddScoped<IProductDal, EfProductDal>();
+                                               
+//iac****************************
+//builder.Services.AddScoped<IProductService, ProductManager>();
+//builder.Services.AddScoped<IProductDal, EfProductDal>();
 
 var app = builder.Build();
 
